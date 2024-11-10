6b9862e5f98eb206df5fda70c376393c6c3760c1
HotBowlOfBeanStew <tristanlavoie2011@hotmail.com>
2024-11-10 15:14:02 -0500

    this is my first commit. sub optimised non sub optimal code.

---
+++ gitplease.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void)
+{
+    int ans = 0;
+    do{
+        int ans = 0;
+        printf("Is kate good?(1 = yes, 2 = no): ");
+        scanf("%d",&ans);
+
+        if (ans == 1){
+            printf("yay, you are cool!\n");
+            return -1;
+        }
+        else if (ans == 2){
+            printf("boo, you're weird!\n");
+            return -1;
+        }
+    }while(ans != 1 || ans != 2);
+
+    return 0;
+}
